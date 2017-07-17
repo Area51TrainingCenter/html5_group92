61722c3dc363b38d6963f522f409d0125ae39a1c
Raymi Saldomando <raymi15@gmail.com>
2017-07-16 22:34:43 -0500

    clase_10

--- modulo3/clase_10/ejercicios-clase/Ciclos/script.js
+++ modulo3/clase_10/ejercicios-clase/Ciclos/script.js
@@ -5,19 +5,17 @@ var pregunta1 = prompt('¿Cuántos continentes tiene La Tierra?');
 var pregunta2 = prompt('¿Como se llama el presidente de USA?');
 var pregunta3 = prompt('¿Qué animal hace cuac cuac?');
 
-var respuestas = [5, 'Donald Trump', 'Pato'];
-
 var respuestasCorrectas = 0;
 
-if( parseInt(pregunta1) === respuestas[0] ) {
+if( parseInt(pregunta1) === 5 ) {
   respuestasCorrectas = respuestasCorrectas + 1;
 }
 
-if( pregunta2 === respuestas[1] ){
+if( pregunta2 === 'Donald Trump' ){
   respuestasCorrectas = respuestasCorrectas + 1;
 }
 
-if( pregunta3 === respuestas[2] ) {
+if( pregunta3 === 'Pato' ) {
   respuestasCorrectas = respuestasCorrectas + 1;
 }
 
